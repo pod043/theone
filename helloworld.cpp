d2287829fe7bb93a247364ace60d6ca16ac55a4e
Qpy <2273931936@qq.com>
2022-05-03 15:08:49 +0800

    first commit

---
+++ helloworld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>         
+int main()                  //主函数
+{
+    using namespace std;    //记得写就行
+    cout << "HelloWorld\n"; //打印“helloworld"
+    cout << endl;         //这句是单独打一个回车的意思，因为题目不要求，我们就不打了
+    return 0;
+}
